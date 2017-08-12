259f067ebe2d1e18833af0129ba66726ad9222f4
NeonLightPWO <shadesofblack@mailinator.com>
2017-08-12 22:11:40 +0100

    Add some more Commands

--- modules/0.0.1.bat
+++ modules/0.0.1.bat
@@ -1,28 +1,44 @@
 @Echo off
+Set shadowDir=%cd%
+Cd %currentDir%
 Goto Entry
 Exit
 
 :Entry
 Set /P Command="<%cd%> (~)> "
-If "%Command%"=="" Goto Entry
 If "%Command%"=="""" Goto Entry
-If "%Command%"=="test" Goto Test
+If "%Command%"=="cls" Goto Clear
+If "%Command%"=="clear" Goto Clear
 If "%Command%"=="refresh" Goto Refresh
 If "%Command%"=="logout" Goto Logout
 If "%Command%"=="ls" Goto Ls
-Goto Entry
-
-:Test
-Echo OK
+If "%Command%"=="cd" Goto Cd
+If "%Command%"=="root" Goto Root
 Goto Entry
 
 :Refresh
+Set currentDir=%cd%
+Cd %shadowDir%
 Echo Console Refreshed.
 modules\0.0.1.bat
 
 :Logout
+Cd %shadowDir%
 Startup.bat
 
 :Ls
 Dir
+Goto Entry
+
+:Cd
+Set /P Directory="What directory? "
+Cd %Directory%
+Goto Entry
+
+:Root
+Cd %shadowDir%
+Goto Entry
+
+:Clear
+Cls
 Goto Entry
